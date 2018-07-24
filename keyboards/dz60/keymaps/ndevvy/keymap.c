fea4d6968a8ef4ec7809952ff0f9bd5fde502ed0
Nicole Van Der Horst <nicole@finxpc.com>
2018-07-24 13:17:49 -0700

    tap spacebar for space, hold for layer 1

--- keyboards/dz60/keymaps/ndevvy/keymap.c
+++ keyboards/dz60/keymaps/ndevvy/keymap.c
@@ -6,15 +6,15 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,-----------------------------------------------------------------------------------------.
- * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  back  |      |
+ * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  back  |  |
  * |-----------------------------------------------------------------------------------------+
  * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
  * |-----------------------------------------------------------------------------------------+
  * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
  * |-----------------------------------------------------------------------------------------+
- * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  | MO(RSh, /) | U  | Caps  |
+ * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  | .  | MO(RSh, /)| U  | Caps  |
  * |-----------------------------------------------------------------------------------------+
- * | Ctrl |  Alt  |  Cmd  |              Space                | Cmd | FN |  L  |  D  |  R  |
+ * | Ctrl |  Alt  |  Cmd  |       Tap=Space; Hold=Layer1        | Cmd | FN |  L  |  D  |  R  |
  * `-----------------------------------------------------------------------------------------'
  */
 
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
       KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, ______, MT(MOD_RSFT, KC_SLSH),  KC_UP, KC_CAPS,
-      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, LT(1, KC_SPC), KC_SPC, KC_RGUI, MO(1), KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
 
